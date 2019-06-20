e23a2f4fea7f3b0fb32232e142ee8bf3f9bf49c1
marsmallowz <alsandymaulana15@gmail.com>
2019-06-20 23:28:24 +0700

    Tanda Kutip Print Dan Pembagian

--- h_main.py
+++ h_main.py
@@ -1,16 +1,16 @@
-import h_lexer
-import h_interpreter
-import h_parser
+import bhs_lexer
+import bhs_parser
+import bhs_interpreter
 
 if __name__ == '__main__':
-    lexer = h_lexer.BasicLexer()
-    parser = h_parser.BasicParser()
+    lexer = bhs_lexer.BahasakuLexer()
+    parser = bhs_parser.BahasakuParser()
     env = {}
     while True:
         try:
-            text = input('basic > ')
+            text = input('BHS > ')
         except EOFError:
             break
         if text:
             tree = parser.parse(lexer.tokenize(text))
-            h_interpreter.BasicExecute(tree, env)
+            bhs_interpreter.BahasakuExecute(tree, env)
